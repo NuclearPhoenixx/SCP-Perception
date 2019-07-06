bdd461681cf5f6d649505cc25b14364361370560
Phoenix1747 <apertured@public-files.de>
2019-07-06 22:27:56 +0200

    add scp-017 idle movement, misc

--- src/game/enemies/17/17.gd
+++ src/game/enemies/17/17.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
-onready var nav = get_tree().get_root().find_node("Navigation", true, false)#get_node("../Navigation")
-#onready var player = get_tree().get_root().find_node("Player", true, false)#get_node("../Player")
+onready var nav = get_tree().get_root().find_node("Navigation", true, false)
 onready var pathfinding = get_node("DebugPathfinding")
+onready var idle_timer = get_node("IdleTimer")
 
 var SPEED = 80
+var IDLE_DISTANCE = 120
 var target = null
 var path = PoolVector2Array()
+var idle_move = false
 
 func move(distance):
 	var direction = distance.normalized()
@@ -38,14 +40,21 @@ func open_door(door): #this function will probably kill everybody
 
 func _physics_process(delta):
 	if typeof(target) != 0: #if target node present follow it
+		idle_timer.paused = true
 		dynamic_move()
 	elif path.size() != 0: #if no target node, dont create new path and finish walking path
+		idle_timer.paused = true
 		finish_move()
+	else: #if no target and nowhere left to go activate idle movement
+		idle_timer.paused = false
 	
 	for i in get_slide_count(): #if collision with door - open it
 		var collision = get_slide_collision(i).collider
 		if collision.is_in_group("doors"):
 			open_door(collision)
+		elif idle_move: #if collision while idle moving, clear array to avoid issues
+			idle_move = false
+			path.remove(0)
 	
 	# DEBUG STUFF
 	if settings.debug_mode: #if in debug mode then show the pathfinding vectors
@@ -53,3 +62,10 @@ func _physics_process(delta):
 		for i in path:
 			debug_path.append(to_local(i))
 		pathfinding.points = debug_path
+
+func _on_IdleTimer_timeout(): #crude idle movement code. Can be improved to be more intelligent
+	idle_timer.wait_time = rand_range(2,8)
+	var x = rand_range(-120,120)
+	var y = rand_range(-1,1) * sqrt(pow(IDLE_DISTANCE,2) - pow(x,2))
+	path.append(position + Vector2(x,y))
+	idle_move = true
