f06a8c11fd52140f534af241262aa5c2871be6af
Phoenix1747 <apertured@public-files.de>
2019-07-06 16:38:35 +0200

    finish door

--- src/game/facility/door.gd
+++ src/game/facility/door.gd
@@ -2,5 +2,31 @@ extends StaticBody2D
 
 onready var anim = get_node("DoorModel/DoorAnimation")
 
+var open = false
+
 func _ready():
-	pass
+	set_process_unhandled_key_input(false)
+	get_node("LightOccluderLeft").occluder = get_node("LightOccluderLeft").occluder.duplicate()
+	get_node("LightOccluderRight").occluder = get_node("LightOccluderRight").occluder.duplicate()
+	#get_node("CollisionShape").shape = get_node("CollisionShape").shape.duplicate()
+	#get_node("InteractionArea/CollisionShape2D").shape = get_node("InteractionArea/CollisionShape2D").shape.duplicate()
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("interact") and !anim.is_playing():
+		if open:
+			open = false
+			anim.play("door_anim")
+			#anim.play_backwards("door_anim")
+		else:
+			open = true
+			anim.play_backwards("door_anim")
+			#anim.play("door_anim")
+		
+		get_tree().set_input_as_handled()
+
+func _on_InteractionArea_body_entered(body):
+	if body.name == "Player":
+		set_process_unhandled_key_input(true)
+
+func _on_InteractionArea_body_exited(body):
+	set_process_unhandled_key_input(false)
--- src/game/player/death.gd
+++ src/game/player/death.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
 func _on_DeathZone_body_entered(body): #collision with any enemy will kill you
-	if body.is_in_group("lethal"):
+	if body.is_in_group("lethal") and !settings.debug_mode:
 		print("You died!")
 		get_tree().reload_current_scene()
