0e6ed2243553b65a0e6fa9bd31be51b0d6b35159
Phoenix1747 <apertured@public-files.de>
2019-07-08 16:46:25 +0200

    add item name generation, misc

--- src/game/inventory.gd
+++ src/game/inventory.gd
@@ -1,15 +1,41 @@
 extends Node
 
-var inventory_names = []
-var inventory_paths = []
+var inventory = {}
+var INVENTORY_SIZE = 8 #max number of items in inventory
 
 func pickup(item):
-	inventory_paths.append(item.filename)
-	inventory_names.append(item.name)
+	if inventory.size() < INVENTORY_SIZE:
+		inventory[item.name] = item.filename
+		item.queue_free()
+	else:
+		print("Inventory is full!")
 	
-	item.queue_free()
-	print(inventory_names)
-	print(inventory_paths)
+	print(get_item_names()) #debug
 
 func drop(item):
 	pass
+
+func create_item_name(string): #Ugly as hell, no built-in functions for this. Dont need any more item name dicts though.
+	var numbers = ["0","1","2","3","4","5","6","7","8","9"] #Firstly remove all numbers
+	for n in numbers:
+		string = string.replace(n, "")
+	
+	var i = 0
+	var replace_counter = 0
+	
+	for s in string: #Then add space in front of uppercase char
+		if s.to_lower() != s and i != 0:
+			string = string.insert(i + replace_counter, " ")
+			replace_counter += 1
+		i += 1
+	
+	return string
+
+func get_item_names(): #Grab all the item names in the inventory for viewing in hud
+	var names = []
+	
+	for key in inventory.keys():
+		var name = create_item_name(key)
+		names.append(name)
+		
+	return names
