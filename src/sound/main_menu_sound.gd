e295485d81138f02f5a7a5a1ee82d11cf57eedf7
Phoenix1747 <apertured@public-files.de>
2019-08-02 00:02:01 +0200

    improve code use

--- src/sound/main_menu_sound.gd
+++ src/sound/main_menu_sound.gd
@@ -2,9 +2,6 @@ extends AudioStreamPlayer
 
 onready var the_dread = preload("res://music/menu/the_dread.ogg")
 
-func _ready():
-	connect("finished", self, "loop_theme")
-
 func loop_theme():
 	stream = the_dread
 	play()
