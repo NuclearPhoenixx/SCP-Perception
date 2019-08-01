ab2e1a2b396d208f7fed028e3a8bd8d2ba6f1253
Phoenix1747 <apertured@public-files.de>
2019-08-01 23:57:00 +0200

    random move

--- src/sound/interact_sound.gd
+++ src/sound/interact_sound.gd
@@ -5,15 +5,15 @@ func _ready():
 	inventory.connect("item_dropped", self, "drop_item")
 	game.connect("player_died", self, "player_death")
 
+func player_death(killer_name):
+	var array = sound.death_sounds[killer_name]
+	stream = array[core.rand_int(0,array.size()-1)]
+	play()
+
 func grab_item(): #this sound plays when the player picks up an item
 	stream = sound.item_pickup[0]
 	play()
 
 func drop_item():
 	stream = sound.item_drop[0]
-	play()
-
-func player_death(killer_name):
-	var array = sound.death_sounds[killer_name]
-	stream = array[core.rand_int(0,array.size()-1)]
 	play()
