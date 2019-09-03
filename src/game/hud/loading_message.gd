bdecd5673803b9873b257489b07c9e60297d2ef3
Phoenix1747 <apertured@public-files.de>
2019-09-03 12:47:08 +0200

    Fixed Save/Load message not showing
    Saving and loading was too fast for a human to read the messages so I fixed the visible duration of them

--- src/game/hud/loading_message.gd
+++ src/game/hud/loading_message.gd
@@ -1,8 +1,16 @@
-extends ColorRect
+extends PanelContainer
+
+onready var timer = get_node("Timer")
 
 func _ready():
 	game.connect("loading_started", self, "loading")
-	game.connect("loading_finished", self, "loading")
+	game.connect("loading_finished", self, "launch_timer")
 
 func loading():
-	visible = !visible
+	visible = true
+
+func launch_timer():
+	timer.start()
+
+func _on_Timer_timeout():
+	visible = false
--- src/game/hud/saving_message.gd
+++ src/game/hud/saving_message.gd
@@ -1,8 +1,16 @@
-extends ColorRect
+extends PanelContainer
+
+onready var timer = get_node("Timer")
 
 func _ready():
 	game.connect("saving_started", self, "saving")
-	game.connect("saving_finished", self, "saving")
+	game.connect("saving_finished", self, "launch_timer")
 
 func saving():
-	visible = !visible
+	visible = true
+
+func launch_timer():
+	timer.start()
+
+func _on_Timer_timeout():
+	visible = false
