4ad831fbcd16e6aca4e3d7dc6f121cac54380d3a
Phoenix1747 <apertured@public-files.de>
2019-09-03 13:52:05 +0200

    close inventory if ESC pressed

--- src/game/hud/inventory_hud.gd
+++ src/game/hud/inventory_hud.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _unhandled_key_input(event):
-	if event.is_action_pressed("inventory"): #show/hide inventory
-		#get_tree().set_input_as_handled()
+	#show/hide inventory and close it if ESC pressed
+	if event.is_action_pressed("inventory") or (visible and event.is_action_pressed("ui_cancel")):
+		get_tree().set_input_as_handled()
 		visible = !visible
