f24bed9784059651f5a655097bdebd5f1adebed8
Phoenix1747 <apertured@public-files.de>
2019-09-03 13:31:25 +0200

    move inventory to texture buttons

--- src/game/hud/inventory_slot.gd
+++ src/game/hud/inventory_slot.gd
@@ -1,4 +1,4 @@
-extends ColorRect
+extends TextureButton
 
 onready var NameNode = get_parent().get_parent().get_node("ItemName")
 onready var DescNode = NameNode.get_node("ItemDescription")
@@ -7,15 +7,12 @@ onready var ImgNode = get_node("ItemImage")
 var item_node_name = "" #name of the item's node, useful for dropping item, etc
 var item_name = "" #item name shown in inventory description
 var item_description = "" #item security clearance shown in inventory description
-export(Color, RGBA) var inv_hover_color = Color(1,1,1,0.9)
-export(Color, RGBA) var inv_normal_color = Color(1,1,1,0.5)
 
 func _ready():
 	set_process(false)
 	inventory.connect("item_grabbed", self, "update_inv")
 	inventory.connect("item_dropped", self, "update_inv")
 	inventory.connect("reload_inv", self, "update_inv")
-	color = inv_normal_color
 
 func update_inv():
 	ImgNode.texture = null
@@ -33,22 +30,19 @@ func update_inv():
 			item_name = dict["item_name"]
 			item_node_name = item
 		i += 1
+	
+	NameNode.text = item_name
+	DescNode.text = item_description
 
 func _on_Slot1_gui_input(event):
 	if event.is_action_pressed("drop_item") and !item_node_name.empty():
 		accept_event()
 		inventory.drop(item_node_name)
 
-func _on_Slot1_mouse_entered(): #Mouse hovering background colors
-	color = inv_hover_color
+func _on_Slot1_mouse_entered():
 	NameNode.show()
-	set_process(true)
-
-func _on_Slot1_mouse_exited():
-	color = inv_normal_color
-	NameNode.hide()
-	set_process(false)
-
-func _process(delta):
 	NameNode.text = item_name
 	DescNode.text = item_description
+
+func _on_Slot1_mouse_exited():
+	NameNode.hide()
